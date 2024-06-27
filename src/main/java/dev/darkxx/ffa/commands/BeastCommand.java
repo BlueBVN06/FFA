29df97c65294dea1a750fde52d7aaca1bd7217d3
BlueBVN1716 <60213759+BlueBVN06@users.noreply.github.com>
2024-06-27 14:20:46 +0700

    Create BeastCommand.java

---
+++ src/main/java/dev/darkxx/ffa/commands/BeastCommand.java
@@ -0,0 +1,24 @@
+package dev.darkxx.ffa.commands;
+
+import dev.darkxx.ffa.Main;
+import dev.darkxx.ffa.settings.menu.BeastMenu;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class SettingsCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            return true;
+        }
+
+        Player player = (Player) sender;
+
+        BeastMenu.createBeastMenu(player, Main.getInstance()).open(player);
+
+        return true;
+    }
