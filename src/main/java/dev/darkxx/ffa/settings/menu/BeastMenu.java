e0223be0cdf869b7bb74b93fdc1dc1ebd681a9f2
BlueBVN1716 <60213759+BlueBVN06@users.noreply.github.com>
2024-06-27 17:50:53 +0700

    Update BeastMenu.java

--- src/main/java/dev/darkxx/ffa/settings/menu/BeastMenu.java
+++ src/main/java/dev/darkxx/ffa/settings/menu/BeastMenu.java
@@ -16,6 +16,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
