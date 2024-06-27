75b4512bd50a0be8393e66673d825c9b0e0abdf0
BlueBVN1716 <60213759+BlueBVN06@users.noreply.github.com>
2024-06-27 15:42:57 +0700

    Update BeastCommand.java

--- src/main/java/dev/darkxx/ffa/commands/BeastCommand.java
+++ src/main/java/dev/darkxx/ffa/commands/BeastCommand.java
@@ -8,7 +8,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-public class SettingsCommand implements CommandExecutor {
+public class BeastCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
