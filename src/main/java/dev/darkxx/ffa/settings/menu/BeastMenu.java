9c1c55136d755f35394a804e08711a579738cebd
BlueBVN1716 <60213759+BlueBVN06@users.noreply.github.com>
2024-06-28 14:46:32 +0700

    Update BeastMenu.java

--- src/main/java/dev/darkxx/ffa/settings/menu/BeastMenu.java
+++ src/main/java/dev/darkxx/ffa/settings/menu/BeastMenu.java
@@ -33,7 +33,21 @@ public class BeastMenu extends GuiBuilder {
         super(27);
         this.main = main;
     }
+    public class GuiBuilder {
+    private final Inventory inventory;
 
+    public GuiBuilder(int size, String title) {
+        this.inventory = Bukkit.createInventory(null, size, title);
+    }
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    public int getSize() {
+        return inventory.getSize();
+    }
+    }
     public static GuiBuilder createBeastMenu(Player player, Main main) {
         GuiBuilder inventory = new GuiBuilder(3 * 9, formatColors("Spawn selector"));
 
