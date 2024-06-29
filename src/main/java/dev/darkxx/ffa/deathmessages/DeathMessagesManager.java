773abde0e10e02cfef0828ffe543c1309f1ac414
BlueBVN1716 <60213759+BlueBVN06@users.noreply.github.com>
2024-06-29 11:27:29 +0700

    Update DeathMessagesManager.java
    import org.bukkit.attribute.Attribute;

--- src/main/java/dev/darkxx/ffa/deathmessages/DeathMessagesManager.java
+++ src/main/java/dev/darkxx/ffa/deathmessages/DeathMessagesManager.java
@@ -6,6 +6,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
