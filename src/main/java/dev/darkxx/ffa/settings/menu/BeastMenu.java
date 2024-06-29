f76042147bfa5aeb0c2817e4e3f45824bd3904d9
BlueBVN1716 <60213759+BlueBVN06@users.noreply.github.com>
2024-06-29 12:03:35 +0700

    Update BeastMenu.java
    óc chó

--- src/main/java/dev/darkxx/ffa/settings/menu/BeastMenu.java
+++ src/main/java/dev/darkxx/ffa/settings/menu/BeastMenu.java
@@ -75,7 +75,7 @@ public class BeastMenu extends GuiBuilder {
         
         ItemStack Center = new ItemBuilderGUI(Material.GRASS_BLOCK)
                 .name(formatColors("&l&bCenter"))
-                .lore(formatColors("&7Players:&l&b " + playerCount), formatColors("  "), formatColors("&7Click to warp to Center"));
+                .lore(formatColors("&7Players:&l&b " + playerCount), formatColors("  "), formatColors("&7Click to warp to Center"))
                 .flags(ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_ENCHANTS)
                 .build();
         inventory.setItem(13, Center, p -> {
